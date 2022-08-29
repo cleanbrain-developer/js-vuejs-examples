4a91d3795f01ec7215d3e5d6ed9940458362af40
cleanbrain-developer <cleanbrain-developer@gmail.com>
2022-08-29 22:05:13 +0900

    2022-08-29 : commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!--
+Modal component with customizable slots and CSS transitions.
+-->
+
+<script setup>
+import DemoModal from './DemoModal.vue'
+import { ref } from 'vue'
+
+const showModal = ref(false)
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <DemoModal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>custom header</h3>
+      </template>
+    </DemoModal>
+  </Teleport>
+</template>
